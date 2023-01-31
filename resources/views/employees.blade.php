18fa82c02e17f7edcae6198a23be89ca794612ad
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2023-02-01 00:03:21 +0800

    Form with Database

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -2,26 +2,27 @@
 @section('title', 'Employees')
 @section('content')
 <div class="container mt-5" >
+<h1 class="mt-2 mb-4"> Basic Information </h1>
 <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
+  <div class="col-md-12">
     <label for="firstname" class="form-label">First name</label>
     <input type="text" class="form-control" id="firstname" placeholder="First Name" required>
     <div class="invalid-feedback">
-      Please enter your first name
+      First name is required
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-12">
     <label for="middlename" class="form-label">Middle Name</label>
     <input type="text" class="form-control" id="middlename" placeholder="Middle Name" required>
     <div class="invalid-feedback">
-      Please enter your middle name
+      Middle name is required
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-12">
     <label for="lastname" class="form-label">Last name</label>
     <input type="text" class="form-control" id="lastname" placeholder="Last Name" required>
     <div class="invalid-feedback">
-      Please enter your last name
+      Last name is required
     </div>
   </div>
   <div class="col-md-4">
@@ -31,41 +32,158 @@
        <button type="button" class="btn btn-success datebtn" data-toggle="#datepicker">🗓</button>
     </div>
     <div class="invalid-feedback">
-      Please provide a birthday
+      Birthday is required
+    </div>
+  </div>
+  <div class="col-md-4" style="margin-left:18%">
+    <label for="gender" class="form-label">Gender</label>
+    <select class="form-select" id="gender" required>
+      <option selected disabled value="">...</option>
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+    </select>
+    <div class="invalid-feedback">
+      Gender is required
     </div>
   </div>
   <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Username</label>
+    <label for="maritalStatus" class="form-label">Marital Status</label>
+    <select class="form-select" id="maritalStatus" required>
+      <option selected disabled value="">...</option>
+      <option value="Single">Single</option>
+      <option value="Married">Married</option>
+      <option value="Widowed">Widowed</option>
+      <option value="Divorced">Divorced</option>
+    </select>
+    <div class="invalid-feedback">
+      Marital Status is required
+    </div>
+  </div>
+  <div class="col-md-5" style="margin-left:18%">
+    <label for="contactNo" class="form-label">Contact No.</label>
     <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
-      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+      <span class="input-group-text" id="contactStart" value="+639">+639</span>
+      <input type="text" class="form-control" id="contactNo" aria-describedby="inputGroupPrepend" required>
       <div class="invalid-feedback">
-        Please choose a username.
+        Contact No. is required
       </div>
     </div>
   </div>
-  <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">City</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
+  <div class="col-md-4">
+    <label for="street" class="form-label">Street</label>
+    <input type="text" class="form-control" id="street" placeholder="Last Name" required>
     <div class="invalid-feedback">
-      Please provide a valid city.
+      Street is required
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">State</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose...</option>
-      <option>...</option>
+  <div class="col-md-4" style="margin-left:18%">
+    <label for="brgyVillage" class="form-label">Barangay / Village</label>
+    <input type="text" class="form-control" id="brgyVillage" placeholder="Last Name" required>
+    <div class="invalid-feedback">
+      Barangay / Village is required
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="municipalityCity" class="form-label">Municipality / City</label>
+    <input type="text" class="form-control" id="municipalityCity" placeholder="Last Name" required>
+    <div class="invalid-feedback">
+      Municipality / City is required
+    </div>
+  </div>
+  <div class="col-md-4" style="margin-left:18%">
+    <label for="province" class="form-label">Province</label>
+    <input type="text" class="form-control" id="province" placeholder="Last Name" required>
+    <div class="invalid-feedback">
+      Province is required
+    </div>
+  </div>
+  <hr>
+  <h1 class="mt-2"> Employment Record </h1>
+  <div class="col-md-4" style="margin-right: 40%">
+    <label for="datehire" class="form-label">Date Hire</label>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="mm/dd/yy" id="hirepicker">
+       <button type="button" class="btn btn-success hirebtn" data-toggle="#hirepicker">🗓</button>
+    </div>
+    <div class="invalid-feedback">
+      Date Hire is required
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="employmentStatus" class="form-label">Employment Status</label>
+    <select class="form-select" id="employmentStatus" required>
+      <option selected disabled value="">...</option>
+      <option value="Regular">Regular</option>
+      <option value="Probationary">Probationary</option>
+      <option value="Contractual">Contractual</option>
     </select>
     <div class="invalid-feedback">
-      Please select a valid state.
+      Employment Status is required
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
+  <div class="col-md-4" style="margin-left:18%">
+    <label for="activeResigned" class="form-label">Active / Resigned</label>
+    <select class="form-select" id="activeResigned" required>
+      <option selected disabled value="">...</option>
+      <option value="Active">Active</option>
+      <option value="Resigned">Resigned</option>
+    </select>
     <div class="invalid-feedback">
-      Please provide a valid zip.
+      Active / Resigned is required
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="jobTitle" class="form-label">Job Title</label>
+    <input type="text" class="form-control" id="jobTitle" required>
+    <div class="invalid-feedback">
+      Job Title is required
+    </div>
+  </div>
+  <div class="col-md-4" style="margin-left: 18%">
+    <label for="dateResign" class="form-label">If resigned, specify the separation date:</label>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="mm/dd/yy" id="resignpicker">
+       <button type="button" class="btn btn-success hirebtn" data-toggle="#resignpicker">🗓</button>
+    </div>
+    <div class="invalid-feedback">
+      Please provide resignation date, if you have chosen "Resigned"
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="rank" class="form-label">Rank</label>
+    <input type="text" class="form-control" id="rank" required>
+    <div class="invalid-feedback">
+      Rank is required
+    </div>
+  </div>
+  <div class="col-md-4" style="margin-left: 18%">
+    <label for="department" class="form-label">Department</label>
+    <input type="text" class="form-control" id="department" required>
+    <div class="invalid-feedback">
+      Department is required
+    </div>
+  </div>
+  <hr>
+  <h1 class="mt-2"> Employment Record </h1>
+  <div class="col-md-4" style="margin-right: 40%">
+    <label for="email" class="form-label">Email Address</label>
+    <input type="text" class="form-control" id="email" required>
+    <div class="invalid-feedback">
+      Email Address is required
+    </div>
+  </div>
+  <div class="col-md-4" style="margin-right: 40%">
+    <label for="empPassword" class="form-label">Password</label>
+      <div class="input-group">
+      <input type="password" class="form-control" id="empPassword" required>
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" id="showPasswordBtn" type="button">
+          <i class="fa fa-eye-slash"></i>
+        </button>
+      </div>
+    </div>
+    <div class="invalid-feedback">
+      Password is required
     </div>
   </div>
   <div class="col-12">
@@ -79,6 +197,7 @@
       </div>
     </div>
   </div>
+  
   <div class="col-12">
     <button class="btn btn-primary" type="submit">Submit form</button>
   </div>
--- resources/views/layouts/navandfoot.blade.php
+++ resources/views/layouts/navandfoot.blade.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
         <!-- Styles -->
         <style>
 
@@ -84,10 +85,8 @@
         (function() {
             'use strict';
             window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
+            let forms = document.getElementsByClassName('needs-validation');
+            let validation = Array.prototype.filter.call(forms, function(form) {
                 form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
@@ -98,7 +97,7 @@
 
                 let submitBtn = form.querySelector('button[type="submit"]');
                 submitBtn.onclick = function() {
-                // Your custom onclick function code here
+                // code nung verification mamaya sa DB
                 console.log('Submit button clicked!');
                 };
             });
@@ -112,6 +111,22 @@
         $('#datepicker').datepicker({
             dateFormat: 'mm/dd/yy'
         });
+        $('.hirebtn').click(function(){
+            $('#hirepicker').datepicker('show');
+        });
+        $('#hirepicker').datepicker({
+            dateFormat: 'mm/dd/yy'
+        });
+    });
+    document.getElementById("showPasswordBtn").addEventListener("click", function() {
+    let passwordInput = document.getElementById("empPassword");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        this.innerHTML = "<i class='fa fa-eye'></i>";
+    } else {
+        passwordInput.type = "password";
+        this.innerHTML = "<i class='fa fa-eye-slash'></i>";
+    }
     });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
